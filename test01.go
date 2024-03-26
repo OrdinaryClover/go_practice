93af6ed113f8981b16e0281ea407a868594ee6e3
woo <Bert_Woo@163.com>
2024-03-26 15:09:05 +0800

    语言特性

--- test01.go
+++ test01.go
@@ -12,7 +12,6 @@ func sum(a int, b int) {
 func leng(a string) {
 	// 定义一个整数切片
 	numbers := []int{1, 2, 3, 4, 5}
-
 	// 使用基本的 for循环遍历切片
 	for i := 1; i < len(numbers); i++ {
 		fmt.Println(numbers[i])
@@ -22,11 +21,9 @@ func leng(a string) {
 func time_str() {
 	// 获取当前时间
 	now := time.Now()
-
 	// 格式化时间，使用指定的布局（YYYYMMDDHHMM）
 	// 注意：Go的时间格式化布局使用特定的引用时间（Mon Jan 2 15:04:05 MST 2006）来确定各部分的顺序
 	formattedTime := now.Format("200601021504")
-
 	// 打印格式化后的时间字符串
 	fmt.Println(formattedTime)
 }
